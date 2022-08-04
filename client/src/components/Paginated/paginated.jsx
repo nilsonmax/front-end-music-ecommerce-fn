7be87a3f85e3eb407dcfbfb56b04a8067a8b83fc
oscar.restrepo@gmail.com <oscar.restrepo2@gmail.com>
2022-08-04 14:24:40 -0500

    feat(components/paginated.jsx): creacion del componente paginated

---
+++ client/src/components/Paginated/paginated.jsx
@@ -0,0 +1,54 @@
+function Paginated({
+  setCurrentPage,
+  currentPage,
+  elementsPerPage,
+  totalElements,
+  paginated,
+}) {
+  const pageNumbers = [];
+
+  for (
+    let number = 1;
+    number <= Math.ceil(totalElements / elementsPerPage);
+    number++
+  ) {
+    pageNumbers.push(number);
+  }
+
+  const forward = (pageNumber) => {
+    setCurrentPage(
+      pageNumber === pageNumbers.length ? pageNumber : pageNumber + 1
+    );
+  };
+
+  const backward = (pageNumber) => {
+    setCurrentPage(pageNumber === 1 ? pageNumber : pageNumber - 1);
+  };
+
+  return (
+    <nav>
+      {pageNumbers[0] && (
+        <div>
+          <ul>
+            <li>
+              <button onClick={() => backward(currentPage)}>Previous</button>
+            </li>
+            {pageNumbers &&
+              pageNumbers.map((number) => (
+                <li>
+                  <button href={number} onClick={() => paginated(number)}>
+                    {number}
+                  </button>
+                </li>
+              ))}
+            <li>
+              <button onClick={() => forward(currentPage)}>Next</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default Paginated;

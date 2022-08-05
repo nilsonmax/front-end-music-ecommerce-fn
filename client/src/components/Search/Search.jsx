ba01ac13fa6e895f16ced2087a2873a0d86ce37e
Nilson Max <nilsonbenitez@hotmail.com>
2022-08-05 11:54:54 -0500

    creando Search cs new con fix

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -24,16 +24,23 @@ export const Search = () => {
 
     return (
         <>
+        
           <SearchContainer >
                 <SubSearchContainer>
                 <div className="relative">
+                <form action=''  onSubmit={handleSubmit}>
                 <input
                     type="text"
                     className="block w-full pl-11 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Search..."
-                /> <svg
+                    onChange={handleChange}
+                    value={name}
+                /> 
+                 </form> 
+                <button className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" onClick={handleSubmit} type='submit' value=''>
+                <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                    // className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -45,7 +52,9 @@ export const Search = () => {
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                     />
                 </svg>
+                </button>
                 </div>
+                
                {/* <button className="px-4 text-white bg-purple-600 rounded-full ">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -62,9 +71,10 @@ export const Search = () => {
                         />
                     </svg>
                 </button> */}
+                
                 </SubSearchContainer>
             </SearchContainer>
-     
+           
           
         </>
     )

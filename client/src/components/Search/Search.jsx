d7804ef05a7027c4e5d5f8b000c433436b395fd9
Nilson Max <nilsonbenitez@hotmail.com>
2022-08-05 00:53:50 -0500

    creando Search css

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getByName} from '../../redux/action';
+import { getByName } from '../../redux/action';
 import { useDispatch } from 'react-redux';
 import { ButtonSearch, SearchContainer, SubSearchContainer } from "./style";
 import { useState } from 'react';
@@ -23,26 +23,49 @@ export const Search = () => {
     }
 
     return (
-
-        <SearchContainer >
-            <SubSearchContainer>
-                <form action='' onSubmit={handleSubmit}>
-                    <input
-                        type="text" className="px-4 py-2 w-80"
-                        placeholder="Search..."
-                        onChange={handleChange}
-                        value={name}
-                        autoComplete='off'
+        <>
+          <SearchContainer >
+                <SubSearchContainer>
+                <div className="relative">
+                <input
+                    type="text"
+                    className="block w-full pl-11 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Search..."
+                /> <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                     />
-                </form>
-                <ButtonSearch onClick={handleSubmit} type='submit' value=''>
-                    <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24">
+                </svg>
+                </div>
+               {/* <button className="px-4 text-white bg-purple-600 rounded-full ">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-5 h-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                    >
                         <path
-                            d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
                     </svg>
-                </ButtonSearch>
-            </SubSearchContainer>
-        </SearchContainer>
+                </button> */}
+                </SubSearchContainer>
+            </SearchContainer>
+     
+          
+        </>
     )
 }

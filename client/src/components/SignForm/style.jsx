76ccbdc02775b66a4fd004c98febc86a84a9aa30
Neider08Urbano <80359162+Neider-Urbano@users.noreply.github.com>
2022-08-05 14:05:59 -0500

    Update style.jsx

--- client/src/components/SignForm/style.jsx
+++ client/src/components/SignForm/style.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import tw  from 'twin.macro'
 
 export const ImgContainer = styled.div`
-  ${tw `h-48 bg-red-300 lg:h-screen`};
+  ${tw `h-48 lg:h-screen`};
 `;
 export const InputContainer = styled.input`
   ${tw `sm: outline-none border border-current rounded-md py-1.5 px-8 mb-3.5`};

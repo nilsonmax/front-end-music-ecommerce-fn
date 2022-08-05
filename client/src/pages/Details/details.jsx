9e757b723d98970fb12645066a55ce6686c5988c
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2022-08-05 12:39:45 -0300

    cambios en app.js/detailsContainer.jsx/details.jsx

---
+++ client/src/pages/Details/details.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DetailsContainer } from "../../containers/details/detailsContainer";
+
+
+
+export default function DetailsPage() {
+    return(
+        <DetailsContainer/>
+    )
+}

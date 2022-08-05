280775f463670a49165a48d6e7f9aa055f374ff5
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2022-08-04 23:09:40 -0300

    cambios en details/app/card

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({id, name, brand, price, img, description, stock, status, categoryId, categoryName}) {
   return (
-    <div key={id}>
+    <Link to={"/instruments/"+id} key={id}>
       <h2>{name}</h2>
       <img src={img}/>
       <h3>{brand}</h3>
@@ -13,6 +14,6 @@ export default function Card({id, name, brand, price, img, description, stock, s
       <h4>{`Stock: ${stock}`}</h4>
       <br></br>
       <br></br>
-    </div>
+    </Link>
   )
 }

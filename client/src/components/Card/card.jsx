3c042be6ccea3e54b044a8d25a6569608116a216
Armando Bermudez <armucode@gmail.com>
2022-08-07 15:34:13 -0500

    Fix(client/app/index/card) css

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { StyledCard, StyledButton } from "./style";
+import { BsCart3 } from "react-icons/bs";
+
 
 export default function Card({id, name, brand, price, img, description, stock, status, categoryId, categoryName}) {
   return (
     <Link to={"/instruments/"+id} key={id}>
-      <h2>{name}</h2>
-      <img src={img} alt={name}/>
-      <h3>{brand}</h3>
-      <h4>{`$${price}`}</h4>
-      <h4>{`${status}`}</h4>
       <br></br>
-      <h4>{`${categoryName}`}</h4>
-      <h4>{`Stock: ${stock}`}</h4>
-      <br></br>
-      <br></br>
+      <StyledCard>
+        <img src={img}/>
+        <h2>{name}</h2>
+        <h2>{`$${price}`}</h2>
+        <p>{`${description.slice(0, 50)}...`}</p>
+        <b>{`Stock:`}</b> <span>{`${stock}`}</span>
+        <b>{`Type:`}</b> <span>{`${categoryName}`}</span>
+        <h3>{brand}</h3>
+        <h3>{`${status}`}</h3>
+        <StyledButton><BsCart3/></StyledButton>
+        <br></br>
+
+      </StyledCard>
     </Link>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
+    colors: {
+      primary: '#070600', // black
+      secondary: '#279AF1', // blue
+      tertiary: '#8c8c8c', // gray
+      background: '#ffffff',// white
+      highlight: '#DB5A42', // orange
+
+    }
   },
   plugins: [],
 }
+

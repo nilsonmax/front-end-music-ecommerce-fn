85defcfc52ae71b417968bd6fbe479735154032a
Armando Bermudez <armucode@gmail.com>
2022-08-09 16:25:08 -0500

    Fix(Components/card) css AB

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -5,6 +5,16 @@ import { BsCart3 } from "react-icons/bs";
 
 
 export default function Card({id, name, brand, price, img, description, stock, status, categoryId, categoryName}) {
+
+  const formattedMoney = 
+  price.toLocaleString('es-us', {
+    style: 'currency',
+    currency: 'COL'
+  })
+  let colMoney = formattedMoney.replace('COL', '$')
+  colMoney = colMoney.replace('.00', '')
+
+
   return (
     <Link to={"/instruments/"+id} key={id}>
       <br></br>
@@ -12,11 +22,17 @@ export default function Card({id, name, brand, price, img, description, stock, s
         <img src={img}/>
         <p>{brand}</p>
         <h2>{name}</h2>
-        <h2>{`$${price}`}</h2>
-        <b>{`Status:`}</b> <span>{`${status}`}</span>
-        <b>{`Type:`}</b> <span>{`${categoryName}`}</span>
-        <StyledButton><BsCart3/></StyledButton>
+        <h3>{`${colMoney}`}</h3>
+        {/* <span>{`USD${price/4500}`}</span> */}
         <br></br>
+        <br></br>
+        {/* <b>{`Status:`}</b> <span>{`${status}`}</span> */}
+        <b>{`Type:`}</b> <span>{`${categoryName}`}</span>
+        <button class="inline-flex items-center h-8 px-5 text-background transition-primary duration-150 bg-secondary rounded-lg focus:shadow-outline hover:bg-primary col-span-2">
+        <span>{`Add Car  `}</span>
+        <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+        </button>
+        <span class=" text-ls font-bold leading-none text-tertiary bg-primary m-4 p-4 rounded absolute bottom-5 left-3" >{status}</span>
       </StyledCard>
     </Link>
   )
--- client/src/components/Card/style.jsx
+++ client/src/components/Card/style.jsx
@@ -3,21 +3,24 @@ import tw from 'twin.macro'
 
 export const StyledCard = styled.div`
   ${tw`
-    h-full
+    h-56
     rounded-lg
+    border-solid
+    border-tertiary
     overflow-hidden
     p-2
     m-1 
     max-w-sm 
     grid-cols-3 
-    gap-1
+    gap-0
     bg-background
     max-w-sm
-    grid gap-1 
-    grid-cols-4
+    grid gap-0.5 
+    grid-cols-6
     leading-tight
     relative
-    shadow-lg
+    drop-shadow-md
+    shadow-inner
     transition-all duration-500
     scale-y-75
   `};
@@ -28,22 +31,32 @@ export const StyledCard = styled.div`
       place-content-center
       justify-center
       row-span-6
-      max-h-56
+      max-h-48
       self-auto
       border-0
       border-secondary
       m-0
-      col-span-2
+      col-span-3
+      block
+
       `};
     }
 
     h2 {
       ${tw`
-      col-span-2
+      col-span-3
       m-0
       p-0
       font-bold    
     `};
+    }
+
+    h3 {
+      ${tw`
+        text-highlight
+        font-bold
+        col-span-2
+      `}
     }
 
     
@@ -64,17 +77,19 @@ export const StyledCard = styled.div`
 
   p {
     ${tw`
+      pt-2
       leading-tight
       text-xs
-      col-span-2
+      col-span-3
       text-tertiary
-    `}
-  }
-
-  span {
-    ${tw`
+      `}
+    }
+    
+    span {
+      ${tw`
       m-0
       p-0
+      col-span-2
     `}
   } 
 
@@ -84,9 +99,10 @@ export const StyledButton = styled.figure`
   ${tw`
     absolute
     bottom-0
-    right-48
+    right-32
     bg-highlight
     p-1
+    pr-10
     m-2
     border-0
     border-secondary

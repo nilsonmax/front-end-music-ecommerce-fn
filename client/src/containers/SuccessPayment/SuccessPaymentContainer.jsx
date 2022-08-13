a93df3f5d2bc81022185c1acfa80d01b24053acd
JuanParraIV <jmparra1993@gmail.com>
2022-08-13 08:46:36 -0500

    Creacion de Success Payment Page y componente

---
+++ client/src/containers/SuccessPayment/SuccessPaymentContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Success from '../../components/paymentSuccess/paymentSucess'
+
+const SuccessPaymentContainer = () => {
+  return (
+    <Success/>
+  )
+}
+
+export default SuccessPaymentContainer

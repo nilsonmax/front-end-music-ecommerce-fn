3fa14867635ee8593ce1f02b6f715c6874ab5f31
AngieVaMo <anvamopa2@gmail.com>
2022-08-15 13:04:32 -0500

    formulario del checkout controlado

--- client/src/components/Checkout/checkout.jsx
+++ client/src/components/Checkout/checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";

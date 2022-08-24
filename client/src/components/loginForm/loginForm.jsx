9d85f38b69de8c3ff902f3a69f3d071b374fb097
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2022-08-24 00:31:17 -0300

    se arreglo la nav... casi toda

--- client/src/components/loginForm/loginForm.jsx
+++ client/src/components/loginForm/loginForm.jsx
@@ -71,7 +71,7 @@ const LoginForm = ({ visible, onClose }) => {
 
   if (!visible) return null;
   return (
-    <div className="fixed inset-0 bg-dark bg-opacity-25 backdrop-blur-sm flex items-center justify-center mt-3">
+    <div className="fixed inset-0 z-50 bg-dark bg-opacity-25 backdrop-blur-sm flex items-center justify-center mt-3">
       <div className=" py-6 sm:py-8 lg:py-12">
         <div className=" bg-white rounded-lg max-w-screen-2xl px-4 md:px-8 mx-auto">
           <form className="max-w-lg  mx-auto bg-white ">
@@ -144,37 +144,7 @@ const LoginForm = ({ visible, onClose }) => {
                 Log in
               </button>
 
-              {/*               <div className="flex justify-center items-center relative">
-                <span className="bg-background text-dark underline  text-sm relative px-4">Log in with social</span>
-              </div> */}
-              {/*               <button className="flex justify-center items-center bg-background hover:bg-primary  active:bg-tertiary  border border-tertiary  focus-visible:ring ring-tertiary  text-dark  text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 px-8 py-3">
-                <svg
-                  className="w-5 h-5 shrink-0"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M23.7449 12.27C23.7449 11.48 23.6749 10.73 23.5549 10H12.2549V14.51H18.7249C18.4349 15.99 17.5849 17.24 16.3249 18.09V21.09H20.1849C22.4449 19 23.7449 15.92 23.7449 12.27Z"
-                    fill="#4285F4"
-                  />
-                  <path
-                    d="M12.2549 24C15.4949 24 18.2049 22.92 20.1849 21.09L16.3249 18.09C15.2449 18.81 13.8749 19.25 12.2549 19.25C9.12492 19.25 6.47492 17.14 5.52492 14.29H1.54492V17.38C3.51492 21.3 7.56492 24 12.2549 24Z"
-                    fill="#34A853"
-                  />
-                  <path
-                    d="M5.52488 14.29C5.27488 13.57 5.14488 12.8 5.14488 12C5.14488 11.2 5.28488 10.43 5.52488 9.71V6.62H1.54488C0.724882 8.24 0.254883 10.06 0.254883 12C0.254883 13.94 0.724882 15.76 1.54488 17.38L5.52488 14.29Z"
-                    fill="#FBBC05"
-                  />
-                  <path
-                    d="M12.2549 4.75C14.0249 4.75 15.6049 5.36 16.8549 6.55L20.2749 3.13C18.2049 1.19 15.4949 0 12.2549 0C7.56492 0 3.51492 2.7 1.54492 6.62L5.52492 9.71C6.47492 6.86 9.12492 4.75 12.2549 4.75Z"
-                    fill="#EA4335"
-                  />
-                </svg>
-                Continue with Google.
-              </button> */}
+              
             </div>
 
             <LoginGoogle />
